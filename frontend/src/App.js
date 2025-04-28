cf82a06e2913ef75d7ed4cbea5d6f6f4bf0ffc24
ringojuice03 <kanecorts@gmail.com>
2025-04-28 22:54:30 +0800

    Re-add frontend as a normal folder

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center-column">
+       
+        <div className="item-row">
+          <span>Introduction Complete</span>
+        </div>
+
+        <div className="item-row">
+          <span>CERTIFICATE OF COMPLETION</span>
+        </div>
+
+        <div className="item-row">
+          <span>Awarded to: Isaac Barbosa</span>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
