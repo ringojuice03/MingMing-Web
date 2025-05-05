00a9832a38d1a45542fcc7e17d090badd1c86e83
ringojuice03 <kanecorts@gmail.com>
2025-05-05 20:55:49 +0800

    urls fix

--- backend/mingming/urls.py
+++ backend/mingming/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', include('mingming_app.urls')),
 ]
