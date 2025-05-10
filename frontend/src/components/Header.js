97c5426ce3df1f397911adc5df3e1b0f0343176b
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-10 22:25:00 +0800

    fix: Change class to className

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,20 +2,21 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
     return(
-        <header>
-            <div class="container">
-                <div class="nav-wrapper">
-                    <img src="/images/logo.png" alt="MingMing Logo"/> 
-                    <nav class="primary-navigation">
-                        <ul aria-label="Primary" role="list" class="nav-list">
+        <header className="primary-header">
+            <div className="container">
+                <div className="nav-wrapper">
+                    <img className="logo" src="/images/logo.png" alt="MingMing Logo"/> 
+                    <nav className="primary-navigation">
+                        <ul aria-label="Primary" role="list" className="nav-list">
                             <Link to="/"><li>Home</li></Link>
                             <Link to="/cookies"><li>Cookies</li></Link>
                             <Link to="/about"><li>About</li></Link>
                         </ul>
                     </nav>
-                    <div class="buttons">
-                        <button>Put cart svg here</button>
-                        <button>Put notification svg here</button>
+                    <div className="buttons">
+                        <button>Cart</button>
+                        <button>Notifs</button>
+                        <button>Profile</button>
                     </div>
                 </div>
             </div>
