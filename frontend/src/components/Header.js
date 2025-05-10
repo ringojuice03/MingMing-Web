a31b8f6a0bd611479adc3a2a1731d37727393b13
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-10 21:57:57 +0800

    feat: Add classes and temporary text to elements

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,16 +5,18 @@ export const Header = () => {
         <header>
             <div class="container">
                 <div class="nav-wrapper">
-                    <img src="/images/logo.png"/>
-                    <nav class="primary-navigatioh">
-                        <ul>
+                    <img src="/images/logo.png" alt="MingMing Logo"/> 
+                    <nav class="primary-navigation">
+                        <ul aria-label="Primary" role="list" class="nav-list">
                             <Link to="/"><li>Home</li></Link>
                             <Link to="/cookies"><li>Cookies</li></Link>
                             <Link to="/about"><li>About</li></Link>
                         </ul>
                     </nav>
-                    <button></button>
-                    <button></button>
+                    <div class="buttons">
+                        <button>Put cart svg here</button>
+                        <button>Put notification svg here</button>
+                    </div>
                 </div>
             </div>
         </header>
