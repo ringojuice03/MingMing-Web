1c0313cc43515cdec8c899e6b9f622f2b01415c7
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-10 23:29:07 +0800

    fix: Add footer-wrapper for flexing columns

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,29 +4,31 @@ export const Footer = () => {
     return (
         <footer className="footer | padding-block-100">
             <div className="container">
-                <div className="first-column">
-                    <Link to="/"><img className="footer-logo" src="images/logo.png"/></Link>
-                    <p>Baked to Perfection, Crafted for Delight</p>
-                    <div className="cta-newsletter">
-                        <p></p>
-                        <svg class="newsletter-icon">
-                            <use xlinkHref="images/newsletter-icon.svg#Group 22"></use>
-                        </svg>
+                <div className="footer-wrapper">
+                    <div className="first-column">
+                        <Link to="/"><img className="footer-logo" src="images/logo.png"/></Link>
+                        <p>Baked to Perfection, Crafted for Delight</p>
+                        <div className="cta-newsletter">
+                            <p>Get our newsletter</p>
+                            <svg class="newsletter-icon">
+                                <use xlinkHref="images/newsletter-icon.svg#Group 22"></use>
+                            </svg>
+                        </div>
                     </div>
-                </div>
-                <div>
                     <div>
-                        <nav className="footer-nav">
+                        <div>
+                            <nav className="footer-nav">
+                                
+                            </nav>
+                        </div>
+                        <div>
 
-                        </nav>
-                    </div>
-                    <div>
-
-                    </div>
-                    <div>
-                        <nav className="footer-nav-socials">
-                        
-                        </nav>
+                        </div>
+                        <div>
+                            <nav className="footer-nav-socials">
+                            
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </div>
