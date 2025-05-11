bf7f0c3747162bc27d67fe26e1f777b362d26976
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-11 18:42:00 +0800

    fix: Apply transition only to hovered image
    The previous version makes it so that when you hover over one image,
    then all images will transition to their respective hovSource.
    
    This new method solves the old problem by adding or removing the hover
    class when the hover state changes. In doing so, only the component you
    hover over will receive/lose the "hovered" class.
    
    The method also utilizes the broswer's capability to render css instead
    of using React to apply inline styles.

--- frontend/src/components/ImageTransition.js
+++ frontend/src/components/ImageTransition.js
@@ -1,20 +1,21 @@
 import {useState} from 'react';
+import '../App.css';
 
 export const ImageTransition = ({ defSource, hovSource, alt }) => {
     const [isHovering, setIsHovering] = useState(false);
 
     return(
-        <div>
+        <div className={`image-wrapper ${isHovering ? "hovered" : ""} `}>
             <img
+                className="defSource"
                 src={defSource}
-                style={{ opacity: isHovering ? 0 : 1, transition: 'opacity 0.3s ease-in-out' }}
                 onMouseEnter={setIsHovering(true)}
                 onMouseLeave={setIsHovering(false)}
                 alt={alt}
             />
             <img
+                className="hovSource"
                 src={hovSource}
-                style={{ opacity: isHovering ? 1 : 0, transition: 'opacity 0.3s ease-in-out' }}
                 alt={alt}
             />
         </div>
