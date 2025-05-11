b61884a79e57741f77d4c2349434dbe9259fcce7
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-11 21:10:56 +0800

    feat: Add long/normal class ternary operator

--- frontend/src/components/ImageTransition.js
+++ frontend/src/components/ImageTransition.js
@@ -1,11 +1,11 @@
 import {useState} from 'react';
 import '../App.css';
 
-export const ImageTransition = ({ defSource, hovSource, alt }) => {
+export const ImageTransition = ({ defSource, hovSource, alt, isLong }) => {
     const [isHovering, setIsHovering] = useState(false);
 
     return(
-        <div className={`image-wrapper ${isHovering ? "hovered" : ""} `}>
+        <div className={`image-wrapper ${isHovering ? "hovered" : ""} ${isLong ? "img-long" : "img-normal"}`}>
             <img
                 className="hovSource"
                 src={hovSource}
